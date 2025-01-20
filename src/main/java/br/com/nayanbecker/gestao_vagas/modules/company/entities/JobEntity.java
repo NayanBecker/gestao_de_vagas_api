e15e04e9d6f8dc1e5e089c44795576f72f001ee9
Nayan Becker <nayan.becker@expressosaomiguel.com.br>
2025-01-20 10:48:30 -0300

    Update JobController to set company ID from request and remove null validation from JobEntity

--- src/main/java/br/com/nayanbecker/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/nayanbecker/gestao_vagas/modules/company/entities/JobEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -38,7 +37,6 @@ public class JobEntity {
     private CompanyEntity companyEntity;
 
     @Column(name = "company_id")
-    @NotNull(message = "Este campo é obrigatório")
     private UUID companyId;
 
     @CreationTimestamp
