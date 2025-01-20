6ca5e2bc214f2719b474d0e7bb2f5c30654f9721
Nayan Becker <nayan.becker@expressosaomiguel.com.br>
2025-01-20 12:41:23 -0300

    Implement candidate authentication feature with JWT, including request/response DTOs and username-based lookup

---
+++ src/main/java/br/com/nayanbecker/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,8 @@
+package br.com.nayanbecker.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(
+    String username,
+    String password
+) {
+    
+}
---
+++ src/main/java/br/com/nayanbecker/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,60 @@
+package br.com.nayanbecker.gestao_vagas.modules.candidate.useCases;
+
+
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.nayanbecker.gestao_vagas.modules.candidate.Entities.CandidateRepository;
+import br.com.nayanbecker.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.nayanbecker.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+    
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException{
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+            .orElseThrow(() -> {
+                throw new UsernameNotFoundException("Candidate not found");
+            });
+        var passwordMatches = this.passwordEncoder
+            .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+            .withIssuer("auth0")
+            .withSubject(candidate.getId().toString())
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withExpiresAt(Instant.now().plus(java.time.Duration.ofHours(2)))
+            .sign(algorithm);
+
+            var authCandidateResponseDTO = AuthCandidateResponseDTO.builder()
+                .access_token(token)
+                .build();   
+            return authCandidateResponseDTO;
+    }
+}
