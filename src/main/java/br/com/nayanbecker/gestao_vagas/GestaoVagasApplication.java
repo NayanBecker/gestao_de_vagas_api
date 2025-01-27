cab548ff82136fcb9879c72a3193089cccff7eec
Nayan Becker <nayan.becker@expressosaomiguel.com.br>
2025-01-27 09:59:56 -0300

    Add OpenAPI definition and update security configuration to permit Swagger endpoints

--- src/main/java/br/com/nayanbecker/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/nayanbecker/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,17 @@ package br.com.nayanbecker.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info=@Info(
+		title="Gestao de Vagas",
+		description="Api de Gestão de vagas",
+		version="1.0.0"
+	)	
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
