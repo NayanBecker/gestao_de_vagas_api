584ead4d911565955351a1a5fcdedfe0e1f26e10
nayanbecker <nayanbecker@gmail.com>
2025-01-30 12:23:40 -0300

    Refactor Swagger configuration and enhance ProfileCandidateResponseDTO with OpenAPI annotations for improved API documentation

--- src/main/java/br/com/nayanbecker/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/nayanbecker/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.nayanbecker.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileCandidateResponseDTO {
-    
-    private String description;
-    private String username;
-    private String email;
-    private String phone;
-    private String name;
-    private UUID id;
+        @Schema(description = "Description of the candidate", example = "Experienced Java Developer")
+        private String description;
+
+        @Schema(description = "Username of the candidate", example = "john_doe")
+        private String username;
+
+        @Schema(description = "Email of the candidate", example = "john.doe@example.com")
+        private String email;
+
+        @Schema(description = "Phone number of the candidate", example = "+1234567890")
+        private String phone;
+
+        @Schema(description = "Name of the candidate", example = "John Doe")
+        private String name;
+
+        private UUID id;
 }
