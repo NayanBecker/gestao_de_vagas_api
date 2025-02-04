011ed3b9075c9a80d06d353a39166b98db8d32c0
nayanbecker <nayanbecker@gmail.com>
2025-02-04 09:53:20 -0300

    Add CompanyNotFoundException

---
+++ src/main/java/br/com/nayanbecker/gestao_vagas/exceptions/CompanyNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.nayanbecker.gestao_vagas.exceptions;
+
+public class CompanyNotFoundException extends RuntimeException {
+    
+    public CompanyNotFoundException() {
+        super("Company not found");
+    }
+}
+
