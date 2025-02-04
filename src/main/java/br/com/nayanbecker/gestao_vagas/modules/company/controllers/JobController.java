83e29177c61759c62509a734f6b236109683c1de
nayanbecker <nayanbecker@gmail.com>
2025-02-04 09:55:06 -0300

    update JobController and exception applied
    Enhance JobController to include Swagger documentation and improve error handling in create job method

--- src/main/java/br/com/nayanbecker/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/nayanbecker/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,7 @@ package br.com.nayanbecker.gestao_vagas.modules.company.controllers;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.nayanbecker.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.nayanbecker.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.nayanbecker.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -23,21 +30,33 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    @PreAuthorize("hasRole('company')")
-    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+    @PreAuthorize("hasRole('COMPANY')")
+    @Operation(summary = "Cadastro de vaga", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = JobEntity.class))
+        })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
-        // jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
-        
-        var jobEntity = JobEntity.builder()
-            .title(createJobDTO.getTitle())
-            .benefits(createJobDTO.getBenefits())
-            .description(createJobDTO.getDescription())
-            .level(createJobDTO.getLevel())
-            .companyId(UUID.fromString(companyId.toString()))
-            .build();
-            
 
-        return this.createJobUseCase.execute(jobEntity);
+        try {
+
+            var jobEntity = JobEntity.builder()
+                    .title(createJobDTO.getTitle())
+                    .benefits(createJobDTO.getBenefits())
+                    .description(createJobDTO.getDescription())
+                    .level(createJobDTO.getLevel())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .build();
+
+            var result = this.createJobUseCase.execute(jobEntity);
+            return ResponseEntity.ok().body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e);
+        }
     }
-    
+
 }
